527a10f948f7a4ee56d27cdfdac6b27d351ff10c
fsjacinto <60415633+fsjacinto@users.noreply.github.com>
2020-11-24 18:50:00 +0800

    Integrated Login Prompt with Main Window
    Edited login, Main, Customers and prompt.
    1) When starting main, goes to login window
    2) Successful login opens main window
    3) Tested a customers prompt in Customers window

--- _init_ver/Main.py
+++ _init_ver/Main.py
@@ -1,9 +1,16 @@
 from Window import window
+from login import login_user
 
-home = window(True)
+# #New code 
+# #Comment this section out and uncomment Previous code to go to window directly
+#1------------------------------------#
+login = login_user(0)
+valid = login.promptWindow.mainloop()
+#1end---------------------------------#
 
-home.root.mainloop()
-
-
-
-    
+# #Previous Code 
+# #Comment this section out and uncomment New code to apply login window
+#2------------------------------------#
+# home = window(True)
+# home.root.mainloop()
+#2end---------------------------------#
--- _init_ver/login.py
+++ _init_ver/login.py
@@ -1,67 +1,65 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk, Image
+from prompt import prompt_box
+from Window import window
 
-class Login:
+class login_user(prompt_box):
+	def __init__(self, promptType):
+		self.promptWindow = Tk()
+		super().__init__(promptType, self.promptWindow)
 
-	def __init__(self):
-
-		self.loginwindow = Tk()
-		self.top = Toplevel()
-		self.top.title("LOGIN SCREEN")
-		self.top.geometry("1280x720")
-
+		self.promptWindow.title("LOGIN SCREEN")
 		self.photo = Image.open('logo1.png')
-		self.photo = self.photo.resize((480, 220), Image.ANTIALIAS)
+		self.photo = self.photo.resize((489, 178), Image.ANTIALIAS)
 		self.photoImg =  ImageTk.PhotoImage(self.photo)
-		self.logo = Label(self.top, image = self.photoImg)
-		self.logo.place(x = 390, y = 45)
+		self.logo = Label(self.body, image = self.photoImg)
+		self.logo.grid(column=0, row=0)
 
-		#Initializes username and password to string; default value is empty string
+		# Initializes username and password to string; default value is empty string
 		self.userName = StringVar()
 		self.password = StringVar()
 
-		#Label and entry for username
-		self.label = Label(self.top, text = "Username:", font = ("Helvetica", 16))
-		self.label.place(x = 570, y = 310)
-		self.textBox1 = Entry(self.top, textvariable = self.userName, width = 30, font = ("Helvetica", 16))
-		self.textBox1.place(x = 450, y = 340)
+		# Label and entry for username
+		self.label = Label(self.body, text = "Username:", font = ("Helvetica", 16))
+		self.label.grid(column=0, row=1)
+		self.textBox1 = Entry(self.body, textvariable = self.userName, width = 30, font = ("Helvetica", 16))
+		self.textBox1.grid(column=0, row=2)
 		
-		#Label and entry for password
-		self.label2 = Label(self.top, text = "Password:", font = ("Helvetica", 16))
-		self.label2.place(x = 570, y = 390)
-		self.textBox2 = Entry(self.top, show = "*" , textvariable = self.password, width = 30, font = ("Helvetica", 16))
-		self.textBox2.place(x = 450, y = 420)
+		# Label and entry for password
+		self.label2 = Label(self.body, text = "Password:", font = ("Helvetica", 16))
+		self.label2.grid(column=0, row=3)
+		self.textBox2 = Entry(self.body, show = "*" , textvariable = self.password, width = 30, font = ("Helvetica", 16))
+		self.textBox2.grid(column=0, row=4)
 
-		#Button for login
-		self.button1 = Button(self.top, text = "Login", fg = "black", bg = "#93c47d", relief = "raised", font = ("Helvetica", 16, "bold"), command = self.validate)
-		self.button1.place(x = 590, y = 510)
+		# Button for login
+		self.button1 = Button(self.body, text = "Login", fg = "black", bg = "#93c47d", relief = "raised", font = ("Helvetica", 16, "bold"), command=self.validate)
+		self.button1.grid(column=0, row=5, pady=20)
+
+		# Label for Error Message
+		self.label3 = Label(self.body, fg = "red", font = ("Helvetica", 14))
 
 	def validate(self):
-
 		#Dictionary for list of users (username : password)
 		users = {'admin': 'admin', 'dev': '2000', 'client': '3000', 'employee': '4000'}
 
 		#Saves the input for username and pasword
 		username = self.userName.get()
 		Pass = self.password.get()
-		
+
 		#Checks if the inputted username and password are correct or not
 		if username in users:
-			if (users[username] == Pass): #If username and password are correct, display main window
-				self.loginwindow.deiconify()
-				self.top.destroy()
+			if (users[username] == Pass):				# If username and password are correct, display main window
+				self.promptWindow.destroy()
+				if(username=='admin'):					# If admin, starts window with admin privileges
+					home = window(True)
+				else:
+					home = window(False)
+				home.root.mainloop()
 			else:
-				self.label3 = Label(self.top, text = "Incorrect Password", fg = "red", font = ("Helvetica", 14))
-				self.label3.place(x = 525, y = 460)
+				self.label3.configure(text="Incorrect Password")		# Displays error if incorrect password
+				self.label3.grid(column=0, row=6)
+
 		else:
-			self.label3 = Label(self.top, text = "Username does not exist", fg = "red", font = ("Helvetica", 14))
-			self.label3.place(x = 530, y = 460)
-
-	def run(self):
-		self.loginwindow.withdraw()
-		self.loginwindow.mainloop()
-
-app = Login()
-app.run()
-
-
+			self.label3.configure(text="Username does not exist")		# Displays error nonexistent username
+			self.label3.grid(column=0, row=6)
