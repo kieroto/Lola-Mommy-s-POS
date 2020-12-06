40172d3c605d190a0dcef26e993331d7bc65be5e
Zian Callao <49685303+kieroto@users.noreply.github.com>
2020-12-06 23:06:47 +0700

    Update Order_process.py
    Created needed functionalities and UI
    
    Still need a little bit of stylization

--- _init_ver/Order_process.py
+++ _init_ver/Order_process.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter.font as font
+from table import table
 
 class order_process(ttk.Frame, Tk):
     
@@ -10,44 +11,102 @@ class order_process(ttk.Frame, Tk):
         self.body = body
 
         #################################################
-        menuFont = font.Font(family='Helvetica', size=20)
+        self.menuFont = font.Font(family='Helvetica', size=20)
 
-        # Create frame for scrollpane
+        # Create frame for scrollpane 
         self.scrollpane = ttk.Frame(self.body)
-        self.scrollpane.grid(column = 0, row = 0, rowspan=23, columnspan=2, sticky=N+S+E+W)
+        self.scrollpane.grid(column = 0, row = 0, rowspan=23, columnspan=8, sticky=N+S+E+W)
 
         # Configure Grid system for scrollpane
-        for i in range(23):
+        for i in range(0,23):
             self.scrollpane.rowconfigure(i, weight=1)
       
         #
-        self.scrollpane.columnconfigure(0, weight=10)
-        self.scrollpane.columnconfigure(1, weight=1)
-        
-        # Create scrollbar widget
-        scrollbar = Scrollbar(self.scrollpane, orient="vertical")
-        scrollbar.grid(row=0, column=1, rowspan=23, sticky=N+S+E+W)
-        
-        # Create Listbox widget
-        self.listbox = Listbox(self.scrollpane, width=8)
-        # Attach scrollbar to the listbox widget
-        self.listbox.config(yscrollcommand=scrollbar.set)
-        self.listbox.grid(row=0, column=0, columnspan=1, rowspan=23, sticky=N+S+E+W)
-        scrollbar.config(command=self.listbox.yview)
+        for i in range(0,8):
+            self.scrollpane.columnconfigure(0, weight=30)
+        self.scrollpane.columnconfigure(8, weight=1)
 
-        # Populate listbox with _list
-        _list=[]
-        for i in range(22):
-            _list.append('<item> '+ str(i))
-        for item in _list:
-        # insert each new item to the end of the listbox
-            self.listbox.insert('end', item)
-        # optionally scroll to the bottom of the listbox
-        self.listbox['font']=menuFont
+        # Labels
+        ordernum = Label(self.scrollpane, text='Order : #8000', font=('Helvetica', 30, 'bold'))
+        ordernum.grid(column=0, row=0, rowspan=3, sticky=N+W)
+        cust = Label(self.scrollpane, text='Customer : Cole Ang', font=('Helvetica', 30, 'bold'))
+        cust.grid(column=0, row=3, rowspan=3, sticky=N+W)
+        Items = Label(self.scrollpane, text='Items', font=('Helvetica', 20, 'bold'))
+        Items.grid(column=0, row=7, rowspan=1, sticky=N+W)
 
+
+        #
+        self.Ready_cook = Button(self.body, text='Ready', font=('Helvetica', 30, 'bold'), command = self.choose_item)
+        self.Ready_cook.grid(column=0, row=11 , columnspan=4, rowspan=6, sticky=N+S+E+W)
+
+        self.Cooked = Button(self.body, text='Ready', font=('Helvetica', 30, 'bold'), command = self.choose_item)
+        self.Cooked.grid(column=4, row=11 , columnspan=4, rowspan=6, sticky=N+S+E+W)
+
+        self.Chicken = Button(self.body, text='Ready', font=('Helvetica', 30, 'bold'), command = self.choose_item)
+        self.Chicken.grid(column=0, row=17 , columnspan=4, rowspan=6, sticky=N+S+E+W)
+
+        self.Pork = Button(self.body, text='Ready', font=('Helvetica', 30, 'bold'), command = self.choose_item)
+        self.Pork.grid(column=4, row=17 , columnspan=4, rowspan=6, sticky=N+S+E+W)
+
+        # Create frame for scrollpane 
+        self.tableframe = ttk.Frame(self.body)
+        self.tableframe.grid(column = 8, row = 2, rowspan=23, columnspan=5,sticky=N+S+E+W)
+
+        # Button
+        Place_order = Button(self.body, text='Place Order ', font=('Helvetica', 30, 'bold'), command = self.place_order)
+        Place_order.grid(column=8, row=0 , columnspan=3, rowspan=1, sticky=N+S+E+W)
+
+        # Configure Grid system for scrollpane
+        for i in range(0,23):
+            self.tableframe.rowconfigure(i, weight=1)
+
+        for i in range(0,5):
+            self.tableframe.columnconfigure(i, weight=1)
+
+        # Create table
+        row_place = 10
+        col_place = 2
+        self.Table_ = table(frame= self.tableframe, tree_row=row_place, tree_col=col_place, column_id=("Item", "Quantity", "Price", "Total"), height = 15)
+        self.Table_.test()
+
+       # Total
+        Total = Label(self.tableframe, text='Total:    <>', font=('Helvetica', 20, 'bold'))
+        Total.grid(column=2, row=23, columnspan=5, rowspan =1, sticky=N+S+E)
  
     def page_id(self):
         return 4
 
     def click(self, i):
         pass
+
+    def place_order(self):
+        print('prompt')
+
+    def destroy_button(self):
+        self.Ready_cook.destroy()
+        self.Cooked.destroy()
+        self.Chicken.destroy()
+        self.Pork.destroy()
+
+    def choose_item(self):
+        self.destroy_button()
+        # Create scrollbar widget
+        self.scrollbar = Scrollbar(self.scrollpane, orient="vertical")
+        self.scrollbar.grid(row=11, column=8, rowspan=12, sticky=N+S+E+W)
+        
+        # Create Listbox widget
+        self.listbox = Listbox(self.scrollpane, width=8)
+        # Attach scrollbar to the listbox widget
+        self.listbox.config(yscrollcommand=self.scrollbar.set)
+        self.listbox.grid(row=11, column=0, columnspan=2, rowspan=23, sticky=N+S+E+W)    
+        self.scrollbar.config(command=self.listbox.yview)
+
+        # Populate listbox with _list
+        self._list=[]
+        for i in range(22):
+            self._list.append('<item> '+ str(i))
+        for item in self._list:
+        # insert each new item to the end of the listbox
+            self.listbox.insert('end', item)
+        # optionally scroll to the bottom of the listbox
+        self.listbox['font']=self.menuFont
