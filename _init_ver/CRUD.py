1a9d3928d15c73a6b19026144201bfdba2e6c993
unigalacorn <60454853+unigalacorn@users.noreply.github.com>
2021-01-10 01:37:31 +0800

    Add files via upload

--- _init_ver/CRUD.py
+++ _init_ver/CRUD.py
@@ -10,6 +10,8 @@ def product():
             productName text,
             category text,
             price integer,
+            wholesaleprice integer,
+            minWholesale integer,
             stock integer
         ) """)
     con.commit()
@@ -42,23 +44,50 @@ def employee():
     con.close()
 
 def order():
-    pass
+    con = sqlite3.connect('order.db')
+    c = con.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS orderx (
+                orderID INTEGER PRIMARY KEY,
+                userID INTEGER,
+                customerID INTEGER,
+                productID INTEGER,
+                productName text,
+                quantity INTEGER,
+                totalAmount INTEGER,
+                date text,
+                time text
+                ) """)
+    con.commit()
+    con.close()
 
 def history():
-    pass
+    con = sqlite3.connect('history.db')
+    c = con.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS history (
+                        historyID INTEGER PRIMARY KEY,
+                        userID INTEGER,
+                        action text,
+                        date text,
+                        time text
+                    ) """)
+    con.commit()
+    con.close()
+
 
 # ----------------------------------------------- ADD ----------------------------------------------------------------------------------------- #
 
 
-def add_product(productID, productName, category, price, stock):
+def add_product(productID, productName, category, price, wholesaleprice, minWholesale, stock):
     con = sqlite3.connect('product.db')
     c = con.cursor()
-    c.execute("INSERT INTO product VALUES (:productID, :productName, :category, :price, :stock)",
+    c.execute("INSERT INTO product VALUES (:productID, :productName, :category, :price, :wholesaleprice, :minWholesale, :stock)",
             {
                 'productID': productID,
                 'productName': productName,
                 'category': category,
                 'price': price,
+                'wholesaleprice': wholesaleprice,
+                'minWholesale': minWholesale,
                 'stock': stock
             })
 
@@ -94,11 +123,39 @@ def add_employee(userID, username, password, role):
     con.commit()
     con.close()
 
-def add_order():
-    pass
+def add_order(orderID, userID, customerID, productID, productName, quantity, totalAmount, date, time):
+    con = sqlite3.connect('order.db')
+    c = con.cursor()
+    c.execute("INSERT INTO orderx VALUES (:orderID, :userID, :customerID, :productID, :productName, :quantity, :totalAmount, :date, :time)",
+              {
+                  'orderID': orderID,
+                  'userID': userID,
+                  'customerID': customerID,
+                  'productID': productID,
+                  'productName': productName,
+                  'quantity': quantity,
+                  'totalAmount': totalAmount,
+                  'date': date,
+                  'time': time,
+              })
 
-def add_history():
-    pass
+    con.commit()
+    con.close()
+
+def add_history(historyID, userID, action, date, time):
+    con = sqlite3.connect('history.db')
+    c = con.cursor()
+    c.execute("INSERT INTO employee VALUES (:historyID, :userID, :action, :date, :time)",
+              {
+                  'historyID': historyID,
+                  'userID': userID,
+                  'action': action,
+                  'date': date,
+                  'time': time,
+              })
+
+    con.commit()
+    con.close()
 
 
 # ----------------------------------------------- RETRIEVE FOR VIEW ---------------------------------------------------------------------------------------- #
@@ -128,13 +185,32 @@ def retrieve_employee():
     return rows
 
 def retrieve_order():
-    pass
+    con = sqlite3.connect('order.db')
+    c = con.cursor()
+    c.execute("SELECT * FROM orderx")
+    rows = c.fetchall()
+    con.close()
+    return rows
 
-def retrieve_customer():
-    pass
+def retrieve_history():
+    con = sqlite3.connect('history.db')
+    c = con.cursor()
+    c.execute("SELECT * FROM history")
+    rows = c.fetchall()
+    con.close()
+    return rows
 
 # ----------------------------------------------- UPDATE ELEMENT ------------------------------------------------------------------------------------------ #
+
+#TEMPLATE TO UPDATE ENTRY
+def update_database_name():
+    con = sqlite3.connect('database_name.db')
+    c = con.cursor()
+    con.close()
+
 # ----------------------------------------------- SEARCH ELEMENT ------------------------------------------------------------------------------------------ #
+
+
 # ----------------------------------------------- DELETE ENTRY -------------------------------------------------------------------------------------------- #
 
 
@@ -142,7 +218,13 @@ def retrieve_customer():
 def delete_database_name(id):
     con = sqlite3.connect('database_name.db')
     c = con.cursor()
-    c.execute("DELETE FROM database_name WHERE id=?",id)
+    c.execute("DELETE FROM table_name WHERE primary_id = " + id)
     con.commit()
     con.close()
 
+def delete_prouct(productID):
+    con = sqlite3.connect('product.db')
+    c = con.cursor()
+    c.execute("DELETE FROM product WHERE productID = " + productID)
+    con.commit()
+    con.close()
