5c01d34d9363644c862f5f633a689230b159193a
Zian Callao <49685303+kieroto@users.noreply.github.com>
2021-01-09 23:52:01 +0700

    Update prompt

--- _init_ver/prompt.py
+++ _init_ver/prompt.py
@@ -2,11 +2,9 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.font as font
 from PIL import ImageTk, Image
-from Order_process import order_process
 
 # Default settings for prompts
 class prompt_box():
-
 	def __init__(self, promptType, master):
 		# if prompt is login, make root window
 		if(promptType==0):
@@ -22,12 +20,10 @@ class prompt_box():
 			prompt_width = 400
 			prompt_height = 300
 			# root.eval(f'tk::PlaceWindow {str(second_win)} center')
-
 		# Set default configrations for prompts here
 		#1-------------------------------------------------------------#	
 		# stylePrompt = ttk.Style()
 		# stylePrompt.configure("All", font="Helvetica", fontsize=15)
-
 		# To set window to center of screen
 		screen_width = self.promptWindow.winfo_screenwidth()
 		screen_height = self.promptWindow.winfo_screenheight()
@@ -47,16 +43,22 @@ class prompt_box():
 			self.body.rowconfigure(i, weight=1)
 		#1end-------------------------------------------------------------#
 
+class history(prompt_box):
+		
+	def __init__(self, promptType, body):
+		# btn.configure(state="disabled")
+		super().__init__(promptType, body)
+		self.promptWindow.title("Confirm Customer")
+
+
 class confirm_customer(prompt_box):
 		
 	def __init__(self, promptType, btn, customerDetails, root, body, pages):
 		# btn.configure(state="disabled")
 		super().__init__(promptType, body)
 		self.root = root
-
 		self.promptWindow.title("Confirm Customer")
 		customerName = customerDetails['customerFirst'] + " " + customerDetails['customerLast']
-
 		# Set Labels and Buttons
 		lb1 = Label(self.body, text = "Processing order for: ", font=('Helvetica', 20, 'bold'))
 		lb2 = Label(self.body, text = customerName, font=('Helvetica', 10))
@@ -65,6 +67,7 @@ class confirm_customer(prompt_box):
 		lb5 = Label(self.body, text = " ")
 		cancelBtn = Button(self.body, text="Cancel", command=self.promptWindow.destroy)
 		okBtn = Button(self.body, text="OK", command=lambda:self.confirm(pages))
+		# dropdown = OptionMenu()
 
 		# Place Labels and Buttons on grid
 		lb1.grid(column=0, row=0, columnspan=9, rowspan=2, sticky=(N, S, E, W))
@@ -74,12 +77,39 @@ class confirm_customer(prompt_box):
 		lb5.grid(column=0, row=5, columnspan=9, rowspan=1, sticky=(N, S, E, W))
 		cancelBtn.grid(column=2, row=6, columnspan=2, rowspan=1, sticky=(N, S, E, W))
 		okBtn.grid(column=5, row=6, columnspan=2, rowspan=1, sticky=(N, S, E, W))
+	def confirm(self, pages):
+		for widget in self.master.winfo_children():
+			widget.destroy()
+		self.promptWindow.destroy()
+		from Order_process import order_process
+		self.orderprocess_= order_process(self.root, self.master, pages)
+
+class add_user(prompt_box):
+
+	def __init__(self, promptType, btn, userDetails, root, body, pages):
+		super().__init__(promptType, body)
+		self.root = root
+
+		self.promptWindow.title("User Added")
+
+		lb1 = Label(self.body, text = userDetails['username'], font=('Helvetica', 15, 'bold'))
+		lb2 = Label(self.body, text = "has been added to list of", font=('Helvetica', 10))
+		lb3 = Label(self.body, text = userDetails['role'] + "s", font=('Helvetica', 10, 'bold'))
+		okBtn = Button(self.body, text="OK", command=lambda:self.confirm(pages))
+		# lb2 = Label(self.body, text = customerName, font=('Helvetica', 10))
+
+		lb1.grid(column=0, row=0, columnspan=9, rowspan=2, sticky=(N, S, E, W))
+		lb2.grid(column=0, row=2, columnspan=9, rowspan=1, sticky=(N, S, E, W))
+		lb3.grid(column=0, row=3, columnspan=9, rowspan=1, sticky=(N, S, E, W))
+		okBtn.grid(column=5, row=6, columnspan=2, rowspan=1, sticky=(N, S, E, W))
 
 	def confirm(self, pages):
 		for widget in self.master.winfo_children():
 			widget.destroy()
 		self.promptWindow.destroy()
-		self.orderprocess_= order_process(self.root, self.master, pages)
+		from Home import home_page
+		self.backhome= home_page(self.root, self.master,'Home', pages)
+
 
 #IGNORE THIS SECTION
 #3-------------------------------------------------------------#		
