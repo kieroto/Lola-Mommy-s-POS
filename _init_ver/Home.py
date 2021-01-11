c6241271bffc1c4f150fe0ed9c4330b10b6d0482
Zian Callao <49685303+kieroto@users.noreply.github.com>
2021-01-11 18:58:50 +0700

    Home page change to logo

--- _init_ver/Home.py
+++ _init_ver/Home.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.font as font
 from order import orderselect
-
+from PIL import ImageTk, Image
+import os
 class home_page(ttk.Frame, Tk):
     
     def __init__(self, root, body, title, pages):
@@ -20,11 +21,17 @@ class home_page(ttk.Frame, Tk):
         Userlbl.grid(column=4, row=2 , columnspan=6, rowspan=2, sticky=(N))
 
         #create button
-        self.order_page= Button(self.body, text='ORDER', command= lambda: self.click(pages))
-        self.order_page.grid(column=4, row=5 , columnspan=6, rowspan=5, sticky=(N,S,W,E))
-        self.order_page.configure(background='#89aae0')
-        self.order_page['font']=menuFont 
+        # self.order_page= Button(self.body, text='ORDER', command= lambda: self.click(pages))
+        # self.order_page.grid(column=4, row=5 , columnspan=6, rowspan=5, sticky=(N,S,W,E))
+        # self.order_page.configure(background='#89aae0')
+        # self.order_page['font']=menuFont 
 
+        path = os.path.dirname(os.path.abspath(__file__)) + '\logo1.png'
+        self.photo = Image.open(r""+path)
+        self.photo = self.photo.resize((600, 218), Image.ANTIALIAS)
+        self.photoImg =  ImageTk.PhotoImage(self.photo)
+        self.logo = Label(self.body, image = self.photoImg)
+        self.logo.grid(column=6, row=7)
 
     def click(self, pages):
         for widget in self.body.winfo_children():
