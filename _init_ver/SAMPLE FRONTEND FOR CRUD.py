82960cb99b50a2c27191953d5f0635b8fab7b6f4
Zian Callao <49685303+kieroto@users.noreply.github.com>
2021-01-12 14:33:28 +0700

    Update SAMPLE FRONTEND FOR CRUD.py

--- _init_ver/SAMPLE FRONTEND FOR CRUD.py
+++ _init_ver/SAMPLE FRONTEND FOR CRUD.py
@@ -10,12 +10,7 @@ root.title("Sample Frontend")
 CRUD.product() #CREATE OR CONNECT DATABASE
 CRUD.employee()
 CRUD.customer()
-
-productID = StringVar()
-productName = StringVar()
-category = StringVar()
-price = StringVar()
-stock = StringVar()
+CRUD.adjust()
 
 
 #ADD ENTRY TO DATABASE
@@ -29,7 +24,14 @@ def view_o():
     rows = CRUD.retrieve_order()
     for item in rows:
         print(item)
-
+def view_r():
+    rows = CRUD.retrieve_privileges()
+    for item in rows:
+        print(item)
+def view_u():
+    rows = CRUD.retrieve_employee()
+    for item in rows:
+        print(item)
 def del_cs():
     CRUD.delete_customer()
 
@@ -38,12 +40,22 @@ def del_o():
 
 def dummy():
     CRUD.add_customer(0, '-', '-', 0, '-')
+
+def dummy2():
+    CRUD.add_role('Admin', '111111111')
+    CRUD.add_role('Cashier', '111111111')
+    CRUD.add_role('Inventory Staff', '111111111')
+
+def del_u():
+    CRUD.delete_user()
+    CRUD.add_employee(1,1,'Admin')
 CRUD.product()
 CRUD.customer()
 CRUD.employee()
 CRUD.order()
 CRUD.history()
 CRUD.adjust()
+
 
 button1 = Button(root, text="View customers", width=12,bg="red", command=view_cs, font=("arial", 12, "bold"))
 button1.grid(column=0, row=0)
@@ -53,7 +65,15 @@ button2 = Button(text="Delete Customers", width=12, bg="red", command=del_cs, fo
 button2.grid(column=0, row=1)
 button2 = Button(text="Delete Orders", width=12, bg="red", command=del_o, font=("arial", 12, "bold"))
 button2.grid(column=2, row=1)
+button1 = Button(root, text="View users", width=12,bg="red", command=view_u, font=("arial", 12, "bold"))
+button1.grid(column=0, row=2)
+button2 = Button(text="View in roles", width=12, bg="red", command=view_r, font=("arial", 12, "bold"))
+button2.grid(column=2, row=2)
 button2 = Button(text="dummy", width=12, bg="red", command=dummy, font=("arial", 12, "bold"))
 button2.grid(column=2, row=3)
+button2 = Button(text="add roles", width=12, bg="red", command=dummy2, font=("arial", 12, "bold"))
+button2.grid(column=0, row=3)
+button2 = Button(text="del users", width=12, bg="red", command=del_u, font=("arial", 12, "bold"))
+button2.grid(column=0, row=4)
 
 root.mainloop()
